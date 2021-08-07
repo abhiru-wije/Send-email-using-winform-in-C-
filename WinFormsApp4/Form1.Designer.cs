1f06abf7c293d72fd8234b19d4df1e9e9ad8efc0
abhiru-wije <abdwije19972219@gmail.com>
2021-08-07 20:23:46 +0530

    Send emails using winform using C#

---
+++ WinFormsApp4/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿
+namespace WinFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.rtbBody = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtRecipientEmail = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPortNumber = new System.Windows.Forms.TextBox();
+            this.txtSmtpServer = new System.Windows.Forms.TextBox();
+            this.txtSenderPassword = new System.Windows.Forms.TextBox();
+            this.txtSenderEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxSSL = new System.Windows.Forms.CheckBox();
+            this.cbxHtmlBody = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.Panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.Panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1305, 726);
+            this.panel1.TabIndex = 0;
+            // 
+            // Panel2
+            // 
+            this.Panel2.Controls.Add(this.cbxHtmlBody);
+            this.Panel2.Controls.Add(this.cbxSSL);
+            this.Panel2.Controls.Add(this.button2);
+            this.Panel2.Controls.Add(this.label11);
+            this.Panel2.Controls.Add(this.rtbBody);
+            this.Panel2.Controls.Add(this.button1);
+            this.Panel2.Controls.Add(this.label10);
+            this.Panel2.Controls.Add(this.txtSubject);
+            this.Panel2.Controls.Add(this.label9);
+            this.Panel2.Controls.Add(this.label8);
+            this.Panel2.Controls.Add(this.txtRecipientEmail);
+            this.Panel2.Controls.Add(this.label7);
+            this.Panel2.Controls.Add(this.label6);
+            this.Panel2.Controls.Add(this.txtPortNumber);
+            this.Panel2.Controls.Add(this.txtSmtpServer);
+            this.Panel2.Controls.Add(this.txtSenderPassword);
+            this.Panel2.Controls.Add(this.txtSenderEmail);
+            this.Panel2.Controls.Add(this.label5);
+            this.Panel2.Controls.Add(this.label4);
+            this.Panel2.Controls.Add(this.label3);
+            this.Panel2.Controls.Add(this.label2);
+            this.Panel2.Controls.Add(this.label1);
+            this.Panel2.Location = new System.Drawing.Point(16, 17);
+            this.Panel2.Name = "Panel2";
+            this.Panel2.Size = new System.Drawing.Size(1269, 687);
+            this.Panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1152, 593);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Send Email";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.sendmail_click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label11.Location = new System.Drawing.Point(730, 18);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(109, 28);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Email Body";
+            // 
+            // rtbBody
+            // 
+            this.rtbBody.Location = new System.Drawing.Point(730, 69);
+            this.rtbBody.Name = "rtbBody";
+            this.rtbBody.Size = new System.Drawing.Size(516, 471);
+            this.rtbBody.TabIndex = 17;
+            this.rtbBody.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(282, 593);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Browse_click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label10.Location = new System.Drawing.Point(73, 590);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(149, 28);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "File Attachment";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(282, 513);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(366, 27);
+            this.txtSubject.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label9.Location = new System.Drawing.Point(73, 512);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 28);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Subject";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label8.Location = new System.Drawing.Point(30, 452);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 28);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Email Details";
+            // 
+            // txtRecipientEmail
+            // 
+            this.txtRecipientEmail.Location = new System.Drawing.Point(282, 380);
+            this.txtRecipientEmail.Name = "txtRecipientEmail";
+            this.txtRecipientEmail.Size = new System.Drawing.Size(366, 27);
+            this.txtRecipientEmail.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label7.Location = new System.Drawing.Point(73, 376);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(139, 28);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Recipant Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(30, 317);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 28);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Recipant Details";
+            // 
+            // txtPortNumber
+            // 
+            this.txtPortNumber.Location = new System.Drawing.Point(282, 253);
+            this.txtPortNumber.Name = "txtPortNumber";
+            this.txtPortNumber.Size = new System.Drawing.Size(366, 27);
+            this.txtPortNumber.TabIndex = 8;
+            // 
+            // txtSmtpServer
+            // 
+            this.txtSmtpServer.Location = new System.Drawing.Point(282, 193);
+            this.txtSmtpServer.Name = "txtSmtpServer";
+            this.txtSmtpServer.Size = new System.Drawing.Size(366, 27);
+            this.txtSmtpServer.TabIndex = 7;
+            // 
+            // txtSenderPassword
+            // 
+            this.txtSenderPassword.Location = new System.Drawing.Point(282, 129);
+            this.txtSenderPassword.Name = "txtSenderPassword";
+            this.txtSenderPassword.Size = new System.Drawing.Size(366, 27);
+            this.txtSenderPassword.TabIndex = 6;
+            // 
+            // txtSenderEmail
+            // 
+            this.txtSenderEmail.Location = new System.Drawing.Point(282, 70);
+            this.txtSenderEmail.Name = "txtSenderEmail";
+            this.txtSenderEmail.Size = new System.Drawing.Size(366, 27);
+            this.txtSenderEmail.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(73, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 28);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Port Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(73, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 28);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "SMTP Server";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(73, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sender Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(73, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sender Email";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(30, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sender Details";
+            // 
+            // cbxSSL
+            // 
+            this.cbxSSL.AutoSize = true;
+            this.cbxSSL.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cbxSSL.ForeColor = System.Drawing.SystemColors.Control;
+            this.cbxSSL.Location = new System.Drawing.Point(577, 298);
+            this.cbxSSL.Name = "cbxSSL";
+            this.cbxSSL.Size = new System.Drawing.Size(71, 35);
+            this.cbxSSL.TabIndex = 20;
+            this.cbxSSL.Text = "SSL";
+            this.cbxSSL.UseVisualStyleBackColor = true;
+            // 
+            // cbxHtmlBody
+            // 
+            this.cbxHtmlBody.AutoSize = true;
+            this.cbxHtmlBody.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cbxHtmlBody.ForeColor = System.Drawing.SystemColors.Control;
+            this.cbxHtmlBody.Location = new System.Drawing.Point(547, 642);
+            this.cbxHtmlBody.Name = "cbxHtmlBody";
+            this.cbxHtmlBody.Size = new System.Drawing.Size(126, 29);
+            this.cbxHtmlBody.TabIndex = 21;
+            this.cbxHtmlBody.Text = "HTML Body";
+            this.cbxHtmlBody.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1329, 750);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.Panel2.ResumeLayout(false);
+            this.Panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel Panel2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtRecipientEmail;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPortNumber;
+        private System.Windows.Forms.TextBox txtSmtpServer;
+        private System.Windows.Forms.TextBox txtSenderPassword;
+        private System.Windows.Forms.TextBox txtSenderEmail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.RichTextBox rtbBody;
+        private System.Windows.Forms.CheckBox cbxSSL;
+        private System.Windows.Forms.CheckBox cbxHtmlBody;
+    }
+}
+
---
+++ WinFormsApp4/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofdAttachment;
+        string fileName = " ";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse_click(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                ofdAttachment = new OpenFileDialog();
+                ofdAttachment.Filter = "Images(.jpg,.png)|*.png;*.jpg;|Pdf Files|*,pdf";
+                if (ofdAttachment.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = ofdAttachment.FileName;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void sendmail_click(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                //smtp client details
+                //gmail >> smtp server : smtp.gmail.com, port: 587, ssl required
+                //yahoo >> smtp server : smtp.mail.yahoo.com, port:587, ssl reqiured
+                SmtpClient clientDetail = new SmtpClient();
+                clientDetail.Host = "smtp.gmail.com";
+                clientDetail.Port = 587;
+                clientDetail.EnableSsl = true;
+                //clientDetail.Port = Convert.ToInt32(txtPortNumber.Text.Trim());
+                //clientDetail.Host = txtSmtpServer.Text.Trim();
+                //clientDetail.EnableSsl = cbxSSL.Checked;
+                clientDetail.DeliveryMethod = SmtpDeliveryMethod.Network;
+                clientDetail.UseDefaultCredentials = false;
+                clientDetail.Credentials = new NetworkCredential(txtSenderEmail.Text.Trim(), txtSenderPassword.Text.Trim());
+
+                //Message Details
+                MailMessage mailDetails = new MailMessage();
+                mailDetails.From = new MailAddress(txtSenderEmail.Text.Trim());
+                mailDetails.To.Add(txtRecipientEmail.Text.Trim());
+                //for multiple recients
+                //maildetails.To.Add("Another recipient email address");
+                //for bcc
+                //mailDetails.Bcc.Add("bcc email address")
+                mailDetails.Subject = txtSubject.Text.Trim();
+                mailDetails.IsBodyHtml = cbxHtmlBody.Checked;
+                mailDetails.Body = rtbBody.Text.Trim();
+
+                //file attachments
+                if(fileName.Length >0)
+                {
+                    Attachment attachment = new Attachment(fileName);
+                    mailDetails.Attachments.Add(attachment);
+                }
+                clientDetail.Send(mailDetails);
+                MessageBox.Show("Your mail has been sent");
+                fileName = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+    }
+}
